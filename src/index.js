f2c51488e815bbb6adaf88d046e0b1ca1e860d65
Wander <keyleasia@gmail.com>
2022-04-27 19:46:57 -0600

    Added Background Color

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+      <style>{'body { background-color: #8080ff; }'}</style>
   </React.StrictMode>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to GeeksforGeeks</h1>
+        <div>
+            <h1>Keyle's testing page</h1>
+            <i>Testing</i>
+        </div>
     </div>
   );
 };
